9dbc05eb05b306ca6a4f4fe896b4e0bf214fc1d8
masudhabib <habib000777@hotmail.com>
2019-08-14 11:03:30 -0700

    prime number computaion with a method

--- src/remoteTiger/PrimeOrNot_08.java
+++ src/remoteTiger/PrimeOrNot_08.java
@@ -19,4 +19,17 @@ public class PrimeOrNot_08 {
 	        else{
 	            System.out.print("This is not a Prime Number");}
 	    }
+		
+		
+	public static boolean isPrime(int n){
+		for(int i = 2; i<n; i++){
+			if(n%i == 0){
+				return false;
+			}
+		}
+		
+		//if there is no number found which is a ideal? divisor then return it is a prime number
+		return true;
 	}
+}
+	
