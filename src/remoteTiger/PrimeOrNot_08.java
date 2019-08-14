e19cd611b24292d602e1356c2a912980fa42e5f6
Feral <feralpatelj@gmail.com>
2019-08-14 18:56:37 -0400

    commit

--- src/remoteTiger/PrimeOrNot_08.java
+++ src/remoteTiger/PrimeOrNot_08.java
@@ -6,33 +6,26 @@ public class PrimeOrNot_08 {
 
 	public static void main(String[] args) {
 
-		int num, i, count = 0;
-		Scanner s = new Scanner(System.in);
-		System.out.print("Enter a number : ");
-		num = s.nextInt();
-		for (i = 10; i < num; i++) {
-			if (num % i == 0) {
-				count++;
-				break;
-			}
-		}
-		if (count == 0) {
-			System.out.print("This is a Prime Number");
-		} else {
-			System.out.print("This is not a Prime Number");
-		}
+		int i = 1103;
+		System.out.println(isPrime(i));
 	}
 
-	boolean isPrime(int n) {
+	static boolean isPrime(int n) {
+		int count = 0;
 
-		if (n % 2 == 0)
+		if (n % 2 == 0) {
+			System.out.println("Loop ran:" + count);
 			return false;
-
-		for (int i = 3; i * i <= n; i += 2) {
-			if (n % i == 0)
-				return false;
 		}
+
+		for (int i = 3; i < (n/2); i += 2) {
+			count++;
+			if (n % i == 0) {
+				System.out.println("Loop ran:" + count);
+				return false;
+			}
+		}
+		System.out.println("Loop ran:" + count);
 		return true;
 	}
 }
-
