9ea27f0d81f71933876ce267151cc4ea3843ab04
masudhabib <habib000777@hotmail.com>
2019-08-14 13:20:37 -0700

    reviewed code

--- src/remoteTiger/PrimeOrNot_08.java
+++ src/remoteTiger/PrimeOrNot_08.java
@@ -25,12 +25,14 @@ public class PrimeOrNot_08 {
 
 	boolean isPrime(int n) {
 
-		if (n % 2 == 0)
+		if (n % 2 == 0){
 			return false;
+		}
 
-		for (int i = 3; i * i <= n; i += 2) {
-			if (n % i == 0)
+		for (int i = 3; i <= n; i += 2) {
+			if (n % i == 0){
 				return false;
+			}
 		}
 		return true;
 	}
