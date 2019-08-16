ca3a8dd9e2a4a6426eb5c72c2685356e3bcfc1dc
masudhabib <habib000777@hotmail.com>
2019-08-16 15:38:58 -0700

    another

---
+++ test-another/src/main/java/com/whatever/BestOfTheRest.java
@@ -0,0 +1,12 @@
+package com.whatever;
+
+public class BestOfTheRest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("The sweet");
+	}
+
+}
