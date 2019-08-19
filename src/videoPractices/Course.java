53ca9cf9d56985d3e7eda9d02b7e81d5749d8ea3
Feral <feralpatelj@gmail.com>
2019-08-19 19:31:26 -0400

    commit

--- src/videoPractices/Course.java
+++ src/videoPractices/Course.java
@@ -1,5 +1,43 @@
 package videoPractices;
 
-public class Course {
+import java.io.Serializable;
+
+public class Course implements Cloneable, Serializable, Comparable<Course>
+
+{
+
+	private int id;
+
+	public Course(int id) 
+	{
+		super();
+	this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + "]";
+	}
+
+	@Override
+	public int compareTo(Course o) {
+		
+		return this.id - o.getId();
+	}
+	
+	/*
+	 * @Override public int compareTo(Course o) {
+	 * 
+	 * return this.id.compareTo(o.getId()); }
+	 */
+	
 
 }
