e70402a1ab5844caf5b5a0ab236ee790502e7fbf
Feral <feralpatelj@gmail.com>
2019-08-31 14:35:36 -0400

    commit

--- Java8Lambda/src/io/javabrains/Unit1ExerciseSolutionJava7.java
+++ Java8Lambda/src/io/javabrains/Unit1ExerciseSolutionJava7.java
@@ -7,37 +7,78 @@ import java.util.List;
 
 public class Unit1ExerciseSolutionJava7 {
 
-			public static void main(String[] args) {
-				List<Person> people = Arrays.asList(
+	private static final int List = 0;
+	private static final int Person = 0;
 
-						new Person("Charles", "Dickens", 60), 
-						new Person("Mark", "Twain", 42),
-						new Person("Lewis", "Carroll", 51), 
-						new Person("Charlotte", "Bronte", 45),
-						new Person("Matthew", "Arnold", 39)
+	public static void main(String[] args) {
+		List<Person> people = Arrays.asList(
 
-				);
-				
-				//Step 1: Sort List by lastName
-				Collections.sort(people, new Comparator<Person>() {
+				new Person("Charles", "Dickens", 60), new Person("Mark", "Twain", 42),
+				new Person("Lewis", "Carroll", 51), new Person("Charlotte", "Bronte", 45),
+				new Person("Matthew", "Arnold", 39)
 
-					@Override
-					public int compare(T o1, T o2) {
-					return o1.getLastName.compareTo
-					}
-			  
-					
-				});
-				
-				//Step 2: Create a method that prints all elements in the list
-				
-				
-				//Step 3: create a method that prints all people that have last name beginning with C
+		);
+
+		// Step 1: Sort List by lastName
+		Collections.sort(people, new Comparator<Person>() {
+
+			@Override
+			public int compare(Person o1, Person o2) {
+				// TODO Auto-generated method stub
+				return 0;
 			}
 
-		
+		});
 
+		// Step 2: Create a method that prints all elements in the list
+		System.out.println("Printing all persons");
+		printAll(people);
+
+		// Step 3: create a method that prints all people that have last name beginning with C
+		System.out.println("Printing All persons Last Names beginning with C");
+		printConditionally(people, new Condition()
+
+		{
+
+			@Override
+			public boolean test(Person p) {
+
+				return p.getLastName().startsWith("C");
+			}
+
+		});
+		
+		System.out.println("Printing All persons First Names beginning with C");
+		printConditionally(people, new Condition()
+
+		{
+
+			@Override
+			public boolean test(Person p) {
+
+				return p.getFirstName().startsWith("C");
+			}
+
+		});
+	}
+
+	public static void printConditionally(List<Person> people, Condition condition) {
+		for (Person p : people) {
+			if (p.getLastName().startsWith("C")) {
+				System.out.println(p);
+			}
+		}
+	}
+
+	private static void printAll(List<Person> people) {
+		for (Person p : people) {
+			System.out.println(p);
+		}
 
 	}
 
+}
+
+interface Condition {
+	boolean test(Person p);
 }
---
+++ Java8Lambda/src/io/javabrains/Unit1ExerciseSolutionJava8.java
@@ -0,0 +1,9 @@
+package io.javabrains;
+
+public class Unit1ExerciseSolutionJava8 {
+
+	public static void main(String[] args) {
+	
+	}
+
+}
